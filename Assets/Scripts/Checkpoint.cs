c4dcefe227519e6dd452c1c001a7df93b4f8a2b2
Juolite <49065824+Juolite@users.noreply.github.com>
2025-10-03 17:02:04 -0400

    Ghoul enemy and new sprites
    Implemented ghoul entity with hurt-collider and idle animation
    
    Fixed player run animation sprite issues
    Added new sprites to tilemap
    Added checkpoint sprite and some visual effects on use, to show which one is active.

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -1,15 +1,73 @@
 using UnityEngine;
+using System.Collections;
 
 public class Checkpoint : MonoBehaviour
 {
+    static Checkpoint activeCheckpoint;
+    bool amActiveCheckpoint = false;
+    [SerializeField] float cooldownSeconds = 3f;
     Collider2D col;
+    SpriteRenderer spr;
+    bool onCooldown = false;
     void Start()
     {
         col = GetComponent<Collider2D>();
+        spr = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (amActiveCheckpoint && this != activeCheckpoint) { amActiveCheckpoint = false; spr.color = Color.white; }
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player")) other.GetComponent<PlayerController>().SetSpawnPoint(transform.position);
+        if (onCooldown) return;
+
+        if (other.CompareTag("Player")) Activate(other);
+    }
+
+    void Activate(Collider2D playerCol)
+    {
+        PlayerController pcontr = playerCol.GetComponent<PlayerController>();
+        pcontr.SetSpawnPoint(transform.position);
+
+        StartCoroutine(Warmup());
+
+        onCooldown = true;
+        activeCheckpoint = this;
+        amActiveCheckpoint = true;
+        StartCoroutine(EndCooldown());
+    }
+
+    IEnumerator Warmup()
+    {
+        float targetRed = 0.6f;
+
+        spr.color = Color.black;
+
+        while (spr.color.r < targetRed)
+        {
+            float step = Time.deltaTime / 5f;
+            spr.color = new Color(spr.color.r + step, spr.color.g, spr.color.b);
+            yield return null;
+        }
+
+    }
+
+    IEnumerator EndCooldown()
+    {
+        yield return new WaitForSeconds(cooldownSeconds);
+
+        onCooldown = false;
+        if (activeCheckpoint == this)
+        {
+            spr.color = Color.softRed; //'active' checkpoint
+        }
+        else
+        {
+            spr.color = Color.white; //'empty' checkpoint
+        }
+
     }
 }
