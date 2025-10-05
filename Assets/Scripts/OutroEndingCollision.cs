cce82bf5d9fe7fb2d0e5a06d7465847d760df5ce
Gur <gurlal5231@gmail.com>
2025-10-05 13:51:46 -0400

    OutroScene working

--- Assets/Scripts/OutroEndingCollision.cs
+++ Assets/Scripts/OutroEndingCollision.cs
@@ -1,12 +1,32 @@
 using UnityEngine;
- 
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using System.Collections;
 public class OutroEndingCollision : MonoBehaviour
 {
+    [SerializeField] public string NextScene;
+    [SerializeField] public MusicPlayer musicPlayerToStop;
+    bool Lock = false;
     void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.CompareTag("Player"))
+        if (col.CompareTag("Player") && (!Lock))
         {
-            
+            Lock = true;
+            PlayerController.input.Disable();
+            float waitTime = musicPlayerToStop.EndMusic();
+            FadeToFromBlack fadeScript = FindFirstObjectByType<FadeToFromBlack>();
+            if (fadeScript != null)
+            {
+                float timeToWait = Mathf.Max(fadeScript.fadeDuration, waitTime);
+                fadeScript.FadeOut();
+                StartCoroutine(WaitThenLoad(timeToWait + 0.2f));
+            }
+            else StartCoroutine(WaitThenLoad(waitTime + 0.2f));
         }
     }
+    IEnumerator WaitThenLoad(float waitTime)
+        {
+        yield return new WaitForSeconds(waitTime);
+        SceneManager.LoadScene(NextScene, LoadSceneMode.Single); //load new scene
+        }
 }
